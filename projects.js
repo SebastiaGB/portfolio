3db9e059b4e068f509883aeef9c0d275a74696b7
SebastiaGB <gamun747@gmail.com>
2025-09-12 14:30:19 +0200

    v4

--- projects.js
+++ projects.js
@@ -2,16 +2,16 @@
 
 // Array de proyectos
 const projects = [
-  { id: 1,
+ /* { id: 1,
         title: "Aplicación Web con Angular desplegada en AWS",
-        description: "Muestra cómo servir sitios web estáticos o SPA en la nube",
-        details: "Sitio web estático Angular alojado en Amazon S3, distribuido globalmente con CloudFront y protegido con HTTPS mediante AWS Certificate Manager.",
-        image: "imagenes/App_Web_Desplegada_AWS.jpg",
-        technologies: ["Amazon S3", "CloudFront", "Route 53", "Certificate Manager"],
+        description: "",
+        details: "",
+        image: "",
+        technologies: ["", "", "", ""],
         tags: ["Básico"],
-        github: "https://github.com/SebastiaGB",
+        github: "#",
         demo: "#"
-    }
+    }*/
 ];
 
 // Exportar para uso en script.js (solo si es Node.js)
