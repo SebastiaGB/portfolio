c7b63a6612c4409d3212df07fa7283c05d37f610
SebastiaGB <gamun747@gmail.com>
2025-09-13 03:23:34 +0200

    definitive version v1.4

--- projects.js
+++ projects.js
@@ -2,56 +2,16 @@
 
 // Array de proyectos
 const projects = [
-  { id: 1,
+ /* { id: 1,
         title: "Aplicación Web con Angular desplegada en AWS",
         description: "",
         details: "",
-        image: "imagenes/App_Web_Desplegada_AWS.jpg",
+        image: "",
         technologies: ["", "", "", ""],
         tags: ["Básico"],
         github: "#",
         demo: "#"
-    },
-    { id: 2,
-        title: "Aplicación Web con Angular desplegada en AWS",
-        description: "nfjwejfbwejfjwbjwbjf",
-        details: "",
-        image: "imagenes/App_Web_Desplegada_AWS.jpg",
-        technologies: ["nfjwejfkw", "fefffefe", "fefeefef", "fefefefefeff"],
-        tags: ["Básico"],
-        github: "#",
-        demo: "#"
-    },
-    { id: 3,
-        title: "Aplicación Web con Angular desplegada en AWS",
-        description: "nfjwejfbwejfjwbjwbnjlnljnjlnlnjlnjlnljnljnnknknlkjnlknlknlknklnklnlknklnlknnlkjf",
-        details: "bfwejbkfjbjjfbekjfjbejbjfbejkbfkjbejkbfjebkfjbekjbkjfbejkbfjkbkjebkjfbekjbfkjebkjbfkjebjkbfkjebjkfbjkebfkjbekjfbjkebkjfbekjbfkjbekjfbkjebfjkebkjefbkjbekjbfkjebkjebfkjebkjfbkjebj",
-        image: "imagenes/App_Web_Desplegada_AWS.jpg",
-        technologies: ["nfjwejfkw", "fefffefe", "fefeefef", "fefefefefeff"],
-        tags: ["Básico"],
-        github: "#",
-        demo: "#"
-    },
-    { id: 4,
-        title: "Aplicación Web con Angular desplegada en AWS",
-        description: "",
-        details: "",
-        image: "imagenes/App_Web_Desplegada_AWS.jpg",
-        technologies: ["", "", "", ""],
-        tags: ["Avanzado"],
-        github: "#",
-        demo: "#"
-    },
-    { id: 5,
-        title: "Aplicación Web con Angular desplegada en AWS",
-        description: "",
-        details: "",
-        image: "imagenes/App_Web_Desplegada_AWS.jpg",
-        technologies: ["", "", "", ""],
-        tags: ["Avanzado"],
-        github: "#",
-        demo: "#"
-    }
+    }*/
 ];
 
 // Exportar para uso en script.js (solo si es Node.js)
