bf95cba7234ef6c3cd83bbf619d8f257549b265b
Sebastià Gamundí Beneyto <144663124+SebastiaGB@users.noreply.github.com>
2025-09-13 19:30:55 +0200

    Update projects.js

--- projects.js
+++ projects.js
@@ -5,7 +5,7 @@ const projects = [
   { id: 1,
         title: "Aplicación Web con Angular desplegada en AWS",
         description: "Despliegue sitio web estatico en el cloud",
-        details: "Sitio web estatico alojado en un bucket S3, distribuido globalmente con CloudFront y protegido mediante un certificado SSH usando el Certificatie Manager",
+        details: "Sitio web estático alojado en un bucket S3, distribuido globalmente con CloudFront y protegido mediante un certificado SSL usando el Certificate Manager.",
         image: "https://SebastiaGB.github.io/portfolio/imagenes/web_desplegada_aws.jpg",
         technologies: ["Route 53", "CloudFront", "Certificate Manager", "S3"],
         tags: ["Básico"],
