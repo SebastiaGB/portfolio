8d005d9695337f49ef15626ca48d9c5ef7ed7cd6
Sebastià Gamundí Beneyto <144663124+SebastiaGB@users.noreply.github.com>
2025-09-22 13:58:17 +0200

    Update projects.js

--- projects.js
+++ projects.js
@@ -6,7 +6,7 @@ const projects = [
         title: "Aplicación Web con Angular desplegada en AWS",
         description: "Despliegue sitio web estatico en el cloud",
         details: "Sitio web estático alojado en un bucket S3, distribuido globalmente con CloudFront y protegido mediante un certificado SSL usando el Certificate Manager.",
-        image: "https://SebastiaGB.github.io/portfolio/imagenes/web_desplegada_aws.jpg",
+        image: "https://SebastiaGB.github.io/portfolio/imagenes/Web_desplegada_AWS.jpg",
         technologies: ["Route 53", "CloudFront", "Certificate Manager", "S3"],
         tags: ["Básico"],
         github: "#",
@@ -16,9 +16,20 @@ const projects = [
         title: "Arquitectura ECS",
         description: "Enfocado en orquestación, escalabilidad y arquitectura de microservicios",
         details: "Despligue escalable de servicios en contenedores usando Amazon ECS, Amazon ALB, en una VPC, con Amazon CloudWatch.",
-        image: "https://SebastiaGB.github.io/portfolio/imagenes/docker_en_aws.jpg",
+        image: "https://SebastiaGB.github.io/portfolio/imagenes/Contenedores_AWS.jpg",
         technologies: ["Amazon ECS", "Amazon CloudWatch", "Amazon ECR", "Amazon VPC", "ALB", "NAT Gateway"],
         tags: ["Avanzado"],
+        github: "#",
+        demo: "#"
+    },
+    {
+        id: 3,
+        title: "Kubernetes en AWS",
+        description: "CI/CD, despliegue con contenedores y uso de múltiples lenguajes y tecnologías",
+        fullDescription: "Aplicación distribuida en clúster de Kubernetes gestionado con múltiples APIs backend en contenedores, base de datos MongoDB y servicios de AWS.",
+        image: "https://SebastiaGB.github.io/portfolio/imagenes/Kubernetes_aplicado_AWS.jpg",
+        technologies: ["Angular", "Docker", "Amazon ECR", "MongoDB", "Amazon SES", "Minikube"],
+        tags: ["Profesional"],
         github: "#",
         demo: "#"
     }
