51063ad6953a83de1cb959f005dada8afdd7c8b9
Sebastià Gamundí Beneyto <144663124+SebastiaGB@users.noreply.github.com>
2025-12-08 01:29:29 +0100

    Update projects.js

--- projects.js
+++ projects.js
@@ -26,7 +26,7 @@ const projects = [
         title: "Arquitectura ECS",
         description: "Enfocado en orquestación, escalabilidad y arquitectura de microservicios",
         details: "Despligue escalable de servicios en contenedores usando Amazon ECS, Amazon ALB, en una VPC, con Amazon CloudWatch.",
-        image: "https://SebastiaGB.github.io/portfolio/imagenes/docker_en_aws_old.jpg",
+        image: "https://SebastiaGB.github.io/portfolio/imagenes/Contenedores_AWS.jpg",
         technologies: ["Amazon ECS", "Amazon CloudWatch", "Amazon ECR", "Amazon VPC", "ALB", "NAT Gateway"],
         tags: ["Avanzado"],
         github: "https://github.com/SebastiaGB/Web_con_docker.git",
